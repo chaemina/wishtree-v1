596d6baceae23887b81ee0fc71035d24f1b09fe1
chaemina <chaemina82@naver.com>
2024-12-22 19:44:28 +0900

    fix

--- wish-tree/shared/constants/position.ts
+++ wish-tree/shared/constants/position.ts
@@ -1,5 +1,5 @@
 export const POSITIONS = [
     { TB: 'top-10', LR: 'right-10' },
     { TB: 'top-0', LR: 'left-10' },
-    { TB: 'top-20', LR: 'left-0' },
+    { TB: 'top-60', LR: 'left-0' },
   ];
